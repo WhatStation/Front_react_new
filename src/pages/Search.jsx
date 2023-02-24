180147c8eb6f86b8d985af20d55963fadce1f63d
journey <takeitez1222@hanyang.ac.kr>
2023-02-24 13:26:56 +0900

    useNavigate 사용한 스크립트를 클론시 리로드 안됨  관련 오류 수정

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -23,6 +23,7 @@ const Search = () => {
            state: {data: response.data,
                   input: searchInput}
            });
+      window.location.reload();
     } catch (error) {
       console.error(error);
     }
